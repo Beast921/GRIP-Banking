dbe276a6d9d63c4d49a3adc9b81b0d58309eb5a9
Chirag <chiragnair03@gmail.com>
2022-07-21 02:31:04 +0530

    done

--- config.php
+++ config.php
@@ -1,3 +1,9 @@
-<?php
+<!-- Development connnection -->
+<!-- <?php
   $conn = mysqli_connect('localhost','root','','grip') or die("UNABLE TO CONNECT - ".mysqli_connect_error());
+?> -->
+
+<!-- Remote db connection -->
+<?php
+  $conn = mysqli_connect('remotemysql.com','2BXTOEObZd','ZWm51sD6Ht','2BXTOEObZd') or die("UNABLE TO CONNECT - ".mysqli_connect_error());
 ?>
