5a9eb0f57d71874ef9cbc997b5c3f2c5ac574d91
Chirag <chiragnair03@gmail.com>
2022-07-20 20:08:43 +0530

    third commit

---
+++ grip.sql
@@ -0,0 +1,79 @@
+-- MariaDB dump 10.19  Distrib 10.4.24-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: grip
+-- ------------------------------------------------------
+-- Server version	10.4.24-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `transaction`
+--
+
+DROP TABLE IF EXISTS `transaction`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `transaction` (
+  `sno` int(3) NOT NULL AUTO_INCREMENT,
+  `sender` text NOT NULL,
+  `receiver` text NOT NULL,
+  `amount` int(8) NOT NULL,
+  `datetime` datetime NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`sno`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `transaction`
+--
+
+LOCK TABLES `transaction` WRITE;
+/*!40000 ALTER TABLE `transaction` DISABLE KEYS */;
+/*!40000 ALTER TABLE `transaction` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` int(3) NOT NULL AUTO_INCREMENT,
+  `name` text NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `balance` int(8) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'PATRICK','patrick@gmail.com',1000000),(2,'TRAVIS','travis@gmail.com',30000),(3,'RYAN','ryan@gmail.com',800000),(4,'LOU','lou@gmail.com',32000),(5,'BRUCE','bruce@gmail.com',6000000),(6,'WALTER','walter@gmail.com',700000),(7,'JESSE','jesse@gmail.com',50000),(8,'SAUL','saul@gmail.com',100000),(9,'MORBIUS','morbius@gmail.com',80000),(10,'BILLY','billy@gmail.com',50000);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-07-20  2:08:42
---
+++ selectedacc.php
@@ -0,0 +1,119 @@
+<?php
+  include "config.php";
+  $flag=0;
+
+  if(isset($_POST['submit'])) {
+    $from=$_GET['id'];
+    $to_id=$_POST['to_id'];
+    $to_name=$_POST['to_name'];
+    $amount=$_POST['amount'];
+
+    $sql="SELECT * FROM user where id='$from'";
+    $query=mysqli_query($conn,$sql);
+    $sql1=mysqli_fetch_array($query);
+
+    $sql="SELECT * FROM user where id='$to_id' and name='$to_name'";
+    $query=mysqli_query($conn,$sql);
+    $sql2=mysqli_fetch_array($query);
+
+    if($amount < 0) {
+      echo '<script type="text/javascript">';
+      echo ' alert("Oops! Negative values cannot be transferred.");';
+      echo '</script>';
+    }
+    else if($amount > $sql1['balance']) {
+      echo '<script type="text/javascript">';
+      echo ' alert("Sorry! Insufficient Balance.");';
+      echo '</script>';
+    }
+    else if($amount==0) {
+      echo '<script type="text/javascript">';
+      echo ' alert("Zero value cannot be transferred.");';
+      echo '</script>';
+    } else {
+      $newbal=$sql1['balance']-$amount;
+      $sql="UPDATE user set balance='$newbal' where id='$from'";
+      mysqli_query($conn,$sql);
+
+      $newbal=$sql2['balance']+$amount;
+      $sql="UPDATE user set balance='$newbal' where id='$to_id'";
+      mysqli_query($conn,$sql);
+
+      $sender=$sql1['name'];
+      $receiver=$sql2['name'];
+      $sql="INSERT INTO transaction(`sender`,`receiver`,`amount`) VALUES ('$sender','$receiver','$amount')";
+      $query=mysqli_query($conn,$sql);
+
+      if($query) {
+        $flag=1;
+      }
+
+      $newbal=0;
+      $amount=0;
+    }
+  }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ABC Bank | Transactions</title>
+  <link rel="stylesheet" href="customers.css">
+</head>
+<body>
+  <?php include "navbar.html" ?>
+  <div class="container" id="container1">
+    <h2><u>Transact Money</u></h2>
+    <?php
+      include 'config.php';
+      $id = $_GET['id'];
+      $sql = "SELECT * FROM  user where id=$id";
+      $res = mysqli_query($conn, $sql);
+      if (!$res) {
+          echo "Error : " . $sql . "<br>" . mysqli_error($conn);
+      }
+      $rows = mysqli_fetch_assoc($res);
+    ?>
+    <table>
+      <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>E-Mail</th>
+        <th>Balance</th>
+      </tr>
+      <tr>
+        <td><?php echo $rows['id']; ?></td>
+        <td><?php echo $rows['name']; ?></td>
+        <td><?php echo $rows['email']; ?></td>
+        <td><?php echo $rows['balance']; ?></td>
+      </tr>
+    </table>
+    <form name="transfer" method="POST" class="form">
+      <label>Transfer To:</label>
+      <div class="creds">
+        <label>Account ID: </label>
+        <input type="number" name="to_id" required>
+        <label>Holder's Name: </label>
+        <input type="text" name="to_name" required>
+      </div>
+      <label>Amount: </label>
+      <input type="number" name="amount" required>
+      <div style="text-align: center;">
+        <button type="submit" name="submit" class="tran-btn" style="font-size:1.3rem; height:2rem; width:6rem;">Transfer</button>
+      </div>
+    </form>
+  </div>
+  <div class="container" id="container2" style="display:none;">
+    <h2>Transaction Successful.</h2>
+  </div>
+  <?php if($flag==1){ ?>
+  <script>
+    document.getElementById("container1").style.display = "none";
+    document.getElementById("container2").style.display = "block";
+  </script>
+  <?php }?>
+</body>
+</html>
