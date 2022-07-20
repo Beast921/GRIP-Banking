c953d408694fcdf05affaa24dc8ceca8939c27eb
Chirag <chiragnair03@gmail.com>
2022-07-20 22:25:30 +0530

    done

--- index.php
+++ index.php
@@ -5,12 +5,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>ABC Bank | Home</title>
-    <link rel="stylesheet" href="home-style.css">
+    <link rel="shortcut icon" href="./images/favicon.ico" type="image/x-icon" />
+    <link rel="stylesheet" href="styles.css">
   </head>
   <body>
     <header>
       <?php include "navbar.html"; ?>
     </header>
-    
+    <div class="container">
+      <h1 class="heading">Welcome to ABC Bank</h1>
+      <h2 style="font-size:3rem">A bank you can fully trust.</h2>
+    </div>
   </body>
 </html>
